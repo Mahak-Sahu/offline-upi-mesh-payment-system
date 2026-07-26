fc3e137e0c622bd1fb0a91bb5715af9e663843cf
Mahak-sahu <mahaksahu808@gmail.com>
2026-07-26 23:48:26 +0530

    Handle and Fix all Frontend part

--- src/main/java/com/demo/upimesh/controller/DashboardController.java
+++ src/main/java/com/demo/upimesh/controller/DashboardController.java
@@ -2,6 +2,7 @@ package com.demo.upimesh.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DashboardController {
@@ -10,4 +11,9 @@ public class DashboardController {
     public String home() {
         return "dashboard";
     }
+    @GetMapping("/test")
+@ResponseBody
+public String test() {
+    return "Hello";
+}
 }
